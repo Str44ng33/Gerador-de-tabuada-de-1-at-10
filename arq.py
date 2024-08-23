5c7c60975e06f97fb2204dc69928c61de7593672
Strang333 <167582329+Str44ng33@users.noreply.github.com>
2024-08-23 09:54:35 -0400

    Create arq.py

---
+++ arq.py
@@ -0,0 +1,29 @@
+#FIZ UM GERADOR DE TABUADA FDS KKKKKKKKKKK
+from colorama import init, Fore
+
+
+init()
+
+num = int(input('Digite o número que tu quer a tabuada (De 1 até 10): '))
+num1 = num * 1
+num2 = num * 2
+num3 = num * 3
+num4 = num * 4
+num5 = num * 5
+num6 = num * 6
+num7 = num * 7
+num8 = num * 8
+num9 = num * 9
+num10 = num * 10
+
+# Imprimindo a tabuada com cores do arco-íris
+print(Fore.RED + f'{num} x 1 é: {num1}')
+print(Fore.YELLOW + f'{num} x 2 é: {num2}')
+print(Fore.GREEN + f'{num} x 3 é: {num3}')
+print(Fore.CYAN + f'{num} x 4 é: {num4}')
+print(Fore.BLUE + f'{num} x 5 é: {num5}')
+print(Fore.MAGENTA + f'{num} x 6 é: {num6}')
+print(Fore.RED + f'{num} x 7 é: {num7}')
+print(Fore.YELLOW + f'{num} x 8 é: {num8}')
+print(Fore.GREEN + f'{num} x 9 é: {num9}')
+print(Fore.CYAN + f'{num} x 10 é: {num10}')
